9ad50ebc3d2beea1cfdfc6dd2567516259d3d91a
FilipKupanovac <filip.kupanovac.11@gmail.com>
2022-04-25 00:01:33 +0200

    Minor improvements

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,2 +1,22 @@
 //image of map, on hover show info about hovered part of map
 //info content: area name, area contents-gyms,markets,etc.
+
+//MISC
+import React, {Component} from 'react'
+//Components
+
+//CSS
+
+class Map extends Component {
+    render(){
+        return(
+            <>
+                <h1>Map</h1>
+                <img src='mapa' alt='ubaci mapu kanto regije'/>
+                <hr/>
+            </>
+        )
+    }
+}
+
+export default Map
--- src/Components/Pokedex.js
+++ src/Components/Pokedex.js
@@ -1,2 +1,24 @@
 //top: filter, searchbar, 
 //result: pokemon image, name, evoution tree, possible encounter area, height, weight
+
+//MISC
+import React, {Component} from 'react'
+//Components
+
+//CSS
+
+class Pokedex extends Component {
+    render(){
+        return(
+            <>
+                <h1>POKEDEX</h1>
+                <input type='text' placeholder='search pokemon'/>
+                <hr/>
+                {/* MAYBE SCROLL COMPONENT HERE */}
+                {/* CARDS */}
+            </>
+        )
+    }
+}
+
+export default Pokedex
