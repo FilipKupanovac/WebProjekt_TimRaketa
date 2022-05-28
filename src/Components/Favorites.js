1927a1d913f6d69457875b572a1522387adcb0b9
FilipKupanovac <filip.kupanovac.11@gmail.com>
2022-05-28 18:21:54 +0200

    Add favorite pokemons

--- src/Components/Favorites.js
+++ src/Components/Favorites.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const Favorites = ({username}) => {
-    return(
-        <p>{username}</p>
-    )
+//COMPONENTS
+import { mockPokedex } from '../mockPokedex';
+import CardList from './CardList';
+
+class Favorites extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            username: this.props.username
+        }
+    }
+    componentDidMount(){
+        //FAKE API CALL - add real later
+        this.setState({pokemons: mockPokedex.results})
+    }
+
+    render(){
+        let {username, pokemons} = this.state;
+
+        return(
+            <>
+                <p>{username}</p>
+                { pokemons !== undefined ?
+                    <CardList 
+                        pokemons={pokemons}
+                        pickedId={0}
+                        pickPokemon={this.fakePick}
+                    />
+                    : 
+                    <></>
+                }
+            </>
+        )
+    }
+    fakePick = (id) => {
+        
+    }
 }
 
 export default Favorites;
