de69e0f758ab47a55ae2d0b91376f57f7b0ca673
bunoza <domagoj.bunoza@gmail.com>
2022-05-30 19:56:48 +0200

    implement login/register using firebase

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,10 +1,14 @@
 //sign in, (username || email) && password
 import React, { Component } from 'react'
 //Components
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
 //CSS
 
 class Signin extends Component {
+
     constructor() {
         super();
         this.state = {
@@ -12,6 +16,7 @@ class Signin extends Component {
             password: ''
         }
     }
+
     render() {
         let { changeCurrentTab } = this.props;
         return (
@@ -44,14 +49,39 @@ class Signin extends Component {
     trySignin = () => {
         //for mock signin only requirements are credentials length > 5 and password = `pokedex`
         let { credentials, password } = this.state;
-        if (credentials.length > 5 && password === 'pokedex') {
-            this.props.loginUser(credentials);
-            this.props.changeCurrentTab('pokedex')
+        if (credentials.length > 5) {
+            var firebaseConfig = {
+                apiKey: "AIzaSyB3chaZc-TREtoMZAgRemjuUozwKXK5xn0",
+                authDomain: "pokedex-4235c.firebaseapp.com",
+                projectId: "pokedex-4235c",
+                storageBucket: "pokedex-4235c.appspot.com",
+                messagingSenderId: "873604095463",
+                appId: "1:873604095463:web:91252982f92892a26db142",
+                measurementId: "G-B8L1LHXNWM"
+            };
+            var firebaseApp = initializeApp(firebaseConfig)
+            let auth = getAuth();
+            var email = credentials
+            signInWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                    // Signed in 
+                    const user = userCredential.user.email;
+                    this.props.loginUser(user.split("@")[0]);
+                    this.props.changeCurrentTab('pokedex')
+                    console.log("made it");
+
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    console.log("ERROR:" + errorMessage);
+                });
+
         }
     }
 
     onInputChange = (key, event) => {
-        console.log(`SET ${key} as: ${event.target.value}`)
+        // console.log(`SET ${key} as: ${event.target.value}`)
         this.setState({ [key]: event.target.value })
     }
 }
